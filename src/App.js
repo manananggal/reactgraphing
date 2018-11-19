d96ccbcd8255d84892c1bbb4eff901a5d4aa53a5
Daedelos Ari Sopmamot-Mendoza <dev.daddyari1@gmail.com>
2018-11-18 22:49:57 -0800

    Convert Vanilla JS app to React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    ratesArray: [],
+  }
+
+  dataFetch = (baseCurrency = 'EUR') => {
+    fetch(`https://api.exchangeratesapi.io/latest?symbols=AUD,BRL,CAD,CHF,GBP&base=${baseCurrency}`)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          currencyData: responseJson,
+        });
+        this.rateLoop();
+      });
+  }
+
+  // Loop through JSON response and create an array of rates
+  rateLoop = () => {
+    const rates = this.state.currencyData.rates;
+
+    // Get max value of rates, to use as bar height ratio
+    const ratesValuesArray = Object.values(rates);
+    const maxRate = Math.max(...ratesValuesArray);
+
+    let ratesArray = [];
+
+    // Create rates as objects
+    for (const key in rates) {
+      const rateObj = {};
+      const val = rates[key];
+      const bar = (val / maxRate) * 100;
+      rateObj.currency = key;
+      rateObj.value = val;
+      rateObj.barHeight = bar;
+      ratesArray.push(rateObj);
+    }
+
+    this.setState({
+      ratesArray: ratesArray,
+    });
+  }
+
+  componentDidMount() {
+    this.dataFetch();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="SelectorSection">
+          <h1>Exchange Rates</h1>
+          <h3>Select a base currency:</h3>
+          <div className="SelectorContainer">
+            <div className="SelectorButtonsContainer">
+              <button className="SelectorButton" onClick={() => this.dataFetch('EUR')}>EUR</button>
+              <button className="SelectorButton" onClick={() => this.dataFetch('USD')}>USD</button>
+            </div>
+          </div>
+        </section>
+
+        <div id="ChartContainer">
+          {
+            this.state.ratesArray.map((item) => (
+              <div key={item.currency} className="ChartBar" style={{height: item.barHeight + '%'}}>
+                <div className="ChartBar-label">{item.currency}</div>
+                <div className="ChartBar-label">{item.value}</div>
+              </div>
+            ))
+          }
+        </div>
       </div>
     );
   }
